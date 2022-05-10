831e9216719cafdbd44c9e8573ef4bc640348a79
neros2k <neros2k@gmail.com>
2022-05-10 14:28:29 +0300

    Аннотации @Deprecated.

--- src/main/java/neros2k/jcapi/JCApi.java
+++ src/main/java/neros2k/jcapi/JCApi.java
@@ -2,6 +2,7 @@ package neros2k.jcapi;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 import java.util.Optional;
+@Deprecated
 public final class JCApi {
     @NotNull
     public static <T> Optional<JSONConfig<T>> getNew(@NotNull JavaPlugin PLUGIN, Class<T> CLASS, String NAME) {
--- src/main/java/neros2k/jcapi/JSONConfig.java
+++ src/main/java/neros2k/jcapi/JSONConfig.java
@@ -1,6 +1,7 @@
 package neros2k.jcapi;
 import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
+@Deprecated
 public final class JSONConfig<T> extends n2k_.jcapi.JSONConfig<T> {
     public JSONConfig(@NotNull Plugin PLUGIN, Class<T> CLASS, String NAME) {
         super(PLUGIN, CLASS, NAME);
