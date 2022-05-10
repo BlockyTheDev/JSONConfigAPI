131597850c3f86a9e4ba7580632eaf6b85f68f7c
neros2k <neros2k@gmail.com>
2022-05-10 14:25:54 +0300

    Некоторые исправления.

--- src/main/java/neros2k/jcapi/JCApi.java
+++ src/main/java/neros2k/jcapi/JCApi.java
@@ -1,2 +1,11 @@
 package neros2k.jcapi;
-public final class JCApi extends n2k_.jcapi.JCApi {}
+import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
+import java.util.Optional;
+public final class JCApi {
+    @NotNull
+    public static <T> Optional<JSONConfig<T>> getNew(@NotNull JavaPlugin PLUGIN, Class<T> CLASS, String NAME) {
+        if(!PLUGIN.isEnabled()) return Optional.empty();
+        return Optional.of(new JSONConfig<>(PLUGIN, CLASS, NAME));
+    }
+}
