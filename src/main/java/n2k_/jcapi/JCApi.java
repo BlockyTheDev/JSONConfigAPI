256b8161922fb14f6cb3801fe8f93a47d8bd93ba
neros2k <neros2k@gmail.com>
2022-05-10 14:26:59 +0300

    Небольшая корректировка.

--- src/main/java/n2k_/jcapi/JCApi.java
+++ src/main/java/n2k_/jcapi/JCApi.java
@@ -2,7 +2,7 @@ package n2k_.jcapi;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 import java.util.Optional;
-public class JCApi extends JavaPlugin {
+public final class JCApi extends JavaPlugin {
     @NotNull
     public static <T> Optional<JSONConfig<T>> getNew(@NotNull JavaPlugin PLUGIN, Class<T> CLASS, String NAME) {
         if(!PLUGIN.isEnabled()) return Optional.empty();
